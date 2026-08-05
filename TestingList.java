61c845f113571b3acdcb2eff651778e843808d4c
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-05 15:28:22 +0530

    Commit

---
+++ TestingList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TestingList {
+    public static void main(String[] args) {
+        
+        List strList = new ArrayList();
+        
+        strList.add("Narayan");
+        System.out.println(strList.get(0));
+
+        
+    }
+}
