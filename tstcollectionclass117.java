124917565077bc120d78d48ec24483bfb204935a
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-06 16:59:40 +0530

    Commit

---
+++ tstcollectionclass117.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class tstcollectionclass117 {
+    public static void main(String[] args) {
+        
+        List<Integer>numList = new ArrayList<>();
+
+        numList.add(15);
+        numList.add(52);
+        numList.add(51);
+        numList.add(25);
+        numList.add(58);
+
+        Utility.printc(numList);
+
+        Collections.sort(numList);
+        System.out.print("Sorted Array : ");
+        Utility.printc(numList);
+    }
+}
