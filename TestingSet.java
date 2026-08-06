fbbe42ff466cc5411e627c6b44237ce7f430aa29
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-06 16:50:04 +0530

    Commit

--- TestingSet.java
+++ TestingSet.java
@@ -1,6 +1,20 @@
+
+import java.util.*;
 public class TestingSet {
     public static void main(String[] args) {
         
-        
+      Set<String> set = new HashSet<>(); 
+
+      System.out.println(set.add("Sanket"));
+      System.out.println(set.add("Bhosale"));
+      System.out.println(set.add("Raje"));
+      
+      System.out.println(set.add("Bhosale"));// Uniqe values only not dublicate values allowed 
+
+    //   System.out.println(set); // not a sireal tyacya manavr ahe kase element show karayache te 
+
+      Utility.printc(set); //use utility class to prin metho
+System.out.println(set.size()); // dublicate bhosale add kela tari pn tyane dublicate element add nay kela 
+// karan allowed ny set madhe dublicate element
     }
 }
