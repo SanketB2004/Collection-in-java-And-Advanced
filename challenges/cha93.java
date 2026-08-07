489e8f098b45ca454723177f8b29219de9f142e6
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-07 16:24:58 +0530

    Commit

---
+++ challenges/cha93.java
@@ -0,0 +1,13 @@
+package challenges;
+
+import java.util.*;
+public class cha93 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
+        System.out.println(list);
+      Collections.reverse(list);
+System.out.println(list);
+
+    }
+
+}
