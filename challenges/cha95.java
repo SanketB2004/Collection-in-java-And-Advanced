8b92e67e8a8096c6e2cfe6cca794d8dc8621fb23
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-07 17:00:53 +0530

    Commit

--- challenges/cha95.java
+++ challenges/cha95.java
@@ -1,5 +1,15 @@
 package challenges;
-
+import java.util.*;
 public class cha95 {
-    
+    public static void main(String[] args) {
+        
+        Set<Character> uniq = new HashSet<>();
+        String userstr = "Hi Sanket Dandesh Zhopala ahe";
+
+        for (Character character : userstr.toCharArray() ) {
+            
+            uniq.add(character);
+        }
+        System.out.println("Unique character in your string : "+ uniq.size());
+    }
 }
