903cdfee1d25a0b593ab853451a333e2d9dcbb80
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-07 15:33:59 +0530

    ommit

--- challenges/cha90.java
+++ challenges/cha90.java
@@ -1,12 +1,20 @@
 package challenges;
+import java.util.*;
 // Write a program that sorts a list od string objects in desending order using a custome comparator .
 
 public class cha90 {
 
     public static void main(String[] args) {
        
+List<String> list = Arrays.asList("Bear", "Lion", "Ant");
+       System.out.println();
         
-        
+
+    }
+
+    public static void sortindecending(List<String> stringList ){
+
+
 
     }
     
