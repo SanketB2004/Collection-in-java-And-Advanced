9deebcfeb513018769688954ec5f89fbc85bb5a3
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-08 16:09:13 +0530

    Commit

--- map/map02.java
+++ map/map02.java
@@ -1,5 +1,5 @@
 package map;
-
+import java.util.*;
 /**
  * map02
  */
@@ -7,7 +7,33 @@ public class map02 {
 
     public static void main(String[] args) {
         
+        Map<String , Long> map = new HashMap<>();
         
+        
+       map.put("Sanket", 7875640607L);
+       map.put("KaKa"  , 9322955586L);
+       map.put("Aai"   , 7756090020L);
+       map.put("Atharv", 9860810865L);
+       map.put("All In One No", 9322955586L);
+
+System.out.println(map.size());
+System.out.println(map.get("Sanket"));
+System.out.println(map.containsKey("Aai"));
+System.out.println(map.containsKey("Don"));
+System.out.println(map.size());
+System.out.println(map.remove("All In One No"));
+System.out.println(map.size());
+
+// all key print in one answer 
+
+for (String string : map.keySet()) {
+    
+    System.out.println(string);
+}
+
+
+       
+
 
     }
     
