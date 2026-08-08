260aaeabbd5ebc89cc936c8a046341ae94d8fcdb
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-08 17:40:48 +0530

    commit

---
+++ challenges/cha97.java
@@ -0,0 +1,27 @@
+package challenges;
+
+public class cha97 {
+
+    enum Day {
+        Mon(true),
+        Tue(true),
+        Wed(true),
+        Thu(true),
+        Fri(true),
+        Sat(false),
+        Sun(false);
+        
+        private final boolean isWeekday;
+
+        Day(boolean isWeekday){
+            this.isWeekday=isWeekday;
+        }
+        
+
+    }
+
+    public static void main(String[] args) {
+
+        
+    }
+}
