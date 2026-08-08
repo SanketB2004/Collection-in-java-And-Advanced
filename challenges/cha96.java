3325113ba876a303c9a3b39c26d5a361f6288549
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-08 17:01:06 +0530

    Commit

--- challenges/cha96.java
+++ challenges/cha96.java
@@ -1,8 +1,24 @@
 package challenges;
 
 public class cha96 {
+
+    enum Days {
+        Mon,
+        Tue,
+        Wed,
+        Thu,
+        Fri,
+        Sat,
+        Sun
+    }
+
     public static void main(String[] args) {
-        
-        
+
+        System.out.println("Printing all the days of the week");
+        System.out.println(Days.Mon);
+
+        for (Days day : Days.values()) {
+            System.out.println(day);
+        }
     }
 }
